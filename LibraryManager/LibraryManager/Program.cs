d4d57ed653fe809b41aff995a567b2b343ddfea3
woorz <marinelangrez@outlook.fr>
2024-01-29 10:24:24 +0100

    feat(libraryManager) : add main

--- LibraryManager/LibraryManager/Program.cs
+++ LibraryManager/LibraryManager/Program.cs
@@ -1,3 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder();
+
+        var host = CreateHostBuilder(configuration).Build();
+        
+        host.Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(IConfigurationBuilder configuration)
+    {
+        return Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                
+            });
+    }
+}
