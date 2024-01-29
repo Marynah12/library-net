5be718c8d1597c7a24a29fdd02717fab0b1730ac
woorz <marinelangrez@outlook.fr>
2024-01-29 13:10:45 +0100

    feat(CatalogService) : updates

--- LibraryManager/Services/Services/CatalogService.cs
+++ LibraryManager/Services/Services/CatalogService.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using BusinessLayer.Catalog;
+using BusinessObjects.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Services.Services
 {
-	public class CatalogService
-	{
-		public CatalogService()
-		{
-		}
-	}
-}
+    public class CatalogService { 
+        private CatalogManager _catalogManager = new CatalogManager();
 
+        public void ShowCatalog()
+        {
+            _catalogManager.DisplayCatalog();
+        }
+
+        public void ShowCatalog(BookType type)
+        {
+            _catalogManager.DisplayCatalog(type);
+        }
+
+
+        public Book FindBook(int id)
+        {
+            return _catalogManager.FindBook(id);
+        }
+
+        
+    }
+}
